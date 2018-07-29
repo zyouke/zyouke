92a58ca66de87667dbbb9bb75d30003884604d58
zyouke <zyouke@aliyun.com>
2018-07-29 22:11:29 +0800

    dubbo 初探 javassist框架

---
+++ framework/src/main/java/com/zyouke/dubbo/javassist/DynamicCreateObject.java
@@ -0,0 +1,31 @@
+package com.zyouke.dubbo.javassist;
+
+import javassist.CannotCompileException;
+import javassist.ClassPool;
+import javassist.CtClass;
+
+import java.io.IOException;
+
+/**
+ * 使用javassist 动态创建类
+ */
+public class DynamicCreateObject {
+
+    public void createSimpleClass(String className){
+        ClassPool classPool = ClassPool.getDefault();
+        CtClass ctClass = classPool.makeClass(className);
+        try {
+            Class clazz = ctClass.toClass();
+            System.out.println("--------------" + clazz.getName());
+            ctClass.writeFile("E://temp//javassist");
+        } catch (CannotCompileException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+
+}
