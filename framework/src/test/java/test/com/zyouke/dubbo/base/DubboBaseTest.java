a989b86087b4473ddf57938763022eeb143e55ea
zhoujun <zhoujun@okay.cn>
2018-08-08 19:23:49 +0800

    dubbo 源码解读---------------->5 内核的解析

--- framework/src/test/java/test/com/zyouke/dubbo/base/DubboBaseTest.java
+++ framework/src/test/java/test/com/zyouke/dubbo/base/DubboBaseTest.java
@@ -1,5 +1,6 @@
 package test.com.zyouke.dubbo.base;
 
+import com.alibaba.dubbo.common.compiler.Compiler;
 import com.alibaba.dubbo.common.extension.ExtensionLoader;
 import com.alibaba.dubbo.registry.RegistryFactory;
 import com.zyouke.dubbo.base.DynamicCreateObject;
@@ -18,7 +19,7 @@ public class DubboBaseTest {
      * @Date: 2017/11/27 10:54
      */
     @Test
-    public void dubboSpiTest() {
+    public void dubboRegistryFactoryTest() {
         ExtensionLoader<RegistryFactory> extensionLoader = ExtensionLoader.getExtensionLoader(RegistryFactory.class);
         RegistryFactory registryFactory = extensionLoader.getAdaptiveExtension();
         RegistryFactory zookeeper = extensionLoader.getExtension("zookeeper");
@@ -31,5 +32,31 @@ public class DubboBaseTest {
     public void createSimpleClassTest(){
         DynamicCreateObject createObject = new DynamicCreateObject();
         createObject.createSimpleClass("Student");
+    }
+
+    @Test
+    public void dubboJavassistCompilerTest(){
+        ExtensionLoader<com.alibaba.dubbo.common.compiler.Compiler> extensionLoader = ExtensionLoader.getExtensionLoader(com.alibaba.dubbo.common.compiler.Compiler.class);
+        Compiler javassistCompiler = extensionLoader.getExtension("javassist");
+        Class<?> compileClass = javassistCompiler.compile("package com.alibaba.dubbo.registry;\n" +
+                "import com.alibaba.dubbo.common.extension.ExtensionLoader;\n" +
+                "public class RegistryFactory$Adpative implements com.alibaba.dubbo.registry.RegistryFactory {\n" +
+                "     public com.alibaba.dubbo.registry.Registry getRegistry(com.alibaba.dubbo.common.URL arg0) {\n" +
+                "        if (arg0 == null) throw new IllegalArgumentException(\"url == null\");\n" +
+                "        com.alibaba.dubbo.common.URL url = arg0;\n" +
+                "        String extName = ( url.getProtocol() == null ? \"dubbo\" : url.getProtocol() );\n" +
+                "        if(extName == null) throw new IllegalStateException(\"Fail to get extension(com.alibaba.dubbo.registry.RegistryFactory) name from url(\" + url.toString() + \") use keys([protocol])\");\n" +
+                "        com.alibaba.dubbo.registry.RegistryFactory extension = (com.alibaba.dubbo.registry.RegistryFactory)ExtensionLoader.getExtensionLoader(com.alibaba.dubbo.registry.RegistryFactory.class).getExtension(extName);\n" +
+                "        return extension.getRegistry(arg0);\n" +
+                "    }\n" +
+                "}\n", ExtensionLoader.class.getClassLoader());
+        try {
+            System.out.println(compileClass);
+            RegistryFactory registryFactory = (RegistryFactory)compileClass.newInstance();
+            System.out.println(registryFactory);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 }
