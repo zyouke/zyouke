eb93b6909fa1e1faee0aeeb2703c20df7b9712be
zhoujun <zhoujun@okay.cn>
2018-08-13 10:24:32 +0800

    dubbo 源码解读---------------->7 spi Activate注解

--- framework/src/main/java/com/zyouke/dubbo/base/spi/job/Teacher.java
+++ framework/src/main/java/com/zyouke/dubbo/base/spi/job/Teacher.java
@@ -1,11 +1,21 @@
 package com.zyouke.dubbo.base.spi.job;
 
+import com.alibaba.dubbo.common.Constants;
 import com.alibaba.dubbo.common.extension.Activate;
 
 /**
  * @Author: zhoujun
  */
-@Activate(group = {"job_group"},order = 1,value = "teacher")
+/**
+ * 使用value时,当url 中存在key为 value时激活,但是不参与排序
+ * url = url.addParameter(Constants.TOKEN_KEY,"teacher");
+ * url 中的key 必须为定义spi 时的key,不然在获取spi的扩展时,因找不到而报错
+ *
+ * 使用before时,表示teacher 在farmer之前
+ * 使用after时,表示teacher 在computer之后
+ * before after 与order混用时按before  after规则
+  */
+ @Activate(group = {"job_group"},before = "farmer",after = "computer")
 public class Teacher implements Job{
     @Override
     public void work() {
