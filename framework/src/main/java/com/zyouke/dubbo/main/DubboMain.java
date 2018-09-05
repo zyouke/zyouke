8dd7bdd0e0122202bc630325681342a43791a44f
zhoujun <zhoujun@okay.cn>
2018-09-05 18:41:51 +0800

    java 动态代理

--- framework/src/main/java/com/zyouke/dubbo/main/DubboMain.java
+++ framework/src/main/java/com/zyouke/dubbo/main/DubboMain.java
@@ -13,18 +13,18 @@ import java.io.IOException;
  * @create 2017/11/16 10:22
  */
 public class DubboMain {
+    // java -cp framework.jar com.zyouke.dubbo.main.DubboMain
     public static void main(String[] args){
         ClassPathXmlApplicationContext context1 = new ClassPathXmlApplicationContext("applicationContext.xml","dubbo/dubbo_provider_1.xml");
         context1.start();
         System.out.println("Dubbo provider1 start...");
+        ClassPathXmlApplicationContext context2 = new ClassPathXmlApplicationContext("applicationContext.xml","dubbo/dubbo_provider_2.xml");
+        context2.start();
+        System.out.println("Dubbo provider2 start...");
         try {
             System.in.read();
         } catch (IOException e) {
             e.printStackTrace();
         }
-       /* ThreadUtil.sleep(3000);
-        ClassPathXmlApplicationContext context2 = new ClassPathXmlApplicationContext("applicationContext.xml","dubbo/dubbo_provider_2.xml");
-        context2.start();
-        System.out.println("Dubbo provider2 start...");*/
     }
 }
--- framework/src/test/java/test/com/zyouke/dubbo/service/ServiceTest.java
+++ framework/src/test/java/test/com/zyouke/dubbo/service/ServiceTest.java
@@ -1,5 +1,7 @@
 package test.com.zyouke.dubbo.service;
 
+import com.alibaba.dubbo.common.bytecode.Proxy;
+import com.alibaba.dubbo.rpc.proxy.InvokerInvocationHandler;
 import com.zyouke.bean.Area;
 import com.zyouke.dubbo.service.AreaServiceImpl;
 import com.zyouke.service.IAreaService;
@@ -18,6 +20,8 @@ public class ServiceTest {
     public void areaServiceTest(){
         ApplicationContext context = new ClassPathXmlApplicationContext("dubbo-consumer.xml");
         IAreaService areaService = (IAreaService) context.getBean("areaService");
+        Proxy bean = (Proxy) context.getBean("areaService");
+        System.out.println("IAreaService 的代理对象的class类型 : " + bean.getClass());
         List<Area> list = areaService.findList(0, 100);
         System.out.println("------------------"+list.toString());
     }
