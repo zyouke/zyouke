1027656f29de215c2a381b65e665c83ee11f8c52
zhoujun <zhoujun@okay.cn>
2018-09-06 17:10:33 +0800

    去掉对于代码,集中对功能的测试

--- framework/src/test/java/test/com/zyouke/dubbo/service/ServiceTest.java
+++ framework/src/test/java/test/com/zyouke/dubbo/service/ServiceTest.java
@@ -19,7 +19,7 @@ public class ServiceTest {
         IDubboService dubboService = (IDubboService) context.getBean("dubboService");
         System.out.println("IDubboService 的代理对象的class类型 : " + dubboService.getClass());
         for (int i = 0; i < 10; i++) {
-            System.out.println("------------------"+dubboService.getExecuteThread());
+            System.err.println("------------------"+dubboService.getExecuteThread());
         }
     }
 
