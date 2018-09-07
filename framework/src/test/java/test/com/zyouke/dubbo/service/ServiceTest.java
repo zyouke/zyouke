df21a24aae4e7b3833a5305d369b0bb31d14deb3
zhoujun <zhoujun@okay.cn>
2018-09-07 16:25:10 +0800

    dubbo 线程池配置

--- framework/src/test/java/test/com/zyouke/dubbo/service/ServiceTest.java
+++ framework/src/test/java/test/com/zyouke/dubbo/service/ServiceTest.java
@@ -18,7 +18,7 @@ public class ServiceTest {
         ApplicationContext context = new ClassPathXmlApplicationContext("dubbo-consumer.xml");
         IDubboService dubboService = (IDubboService) context.getBean("dubboService");
         System.out.println("IDubboService 的代理对象的class类型 : " + dubboService.getClass());
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 1000; i++) {
             System.out.println("------------------"+dubboService.getExecuteThread());
         }
     }
