6d86f9325a073ec9358dc6009aa7728b8090e68c
zyouke <zyouke@aliyun.com>
2018-09-22 22:15:32 +0800

    dubbo 压力测试调试

--- framework/src/main/java/com/zyouke/dubbo/service/DubboServiceImpl.java
+++ framework/src/main/java/com/zyouke/dubbo/service/DubboServiceImpl.java
@@ -10,23 +10,26 @@ import org.springframework.stereotype.Service;
 import java.util.Date;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 /**
  * @Author: zhoujun
  */
 @Service("dubboService")
 public class DubboServiceImpl implements IDubboService{
-
-    AtomicInteger atomicInteger = new AtomicInteger();
+    private Lock lock = new ReentrantLock();
     /**
      * 获取当前执行的线程
      * @return
      */
     @Override
     public String getExecuteThread() {
+        lock.lock();
         String date = DateFormatUtils.format(new Date(), "yyyy-MM-dd HH:mm:ss:ssss");
         int randomNumber = RandomUtil.getRandomNumber(1, 100);
         ThreadUtil.sleep(randomNumber);
+        lock.unlock();
         return ThreadUtil.getThreadName() +"_requestTime : "+ date + "_execute_time :" + randomNumber;
     }
 }
