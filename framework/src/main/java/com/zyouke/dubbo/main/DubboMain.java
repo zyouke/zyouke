3f2c4176fec210a01ec0c327c7ac057e2e6d8e47
zyouke <zyouke@aliyun.com>
2018-10-26 22:41:58 +0800

    添加mybites

--- framework/src/main/java/com/zyouke/dubbo/main/DubboMain.java
+++ framework/src/main/java/com/zyouke/dubbo/main/DubboMain.java
@@ -3,6 +3,7 @@ package com.zyouke.dubbo.main;
 
 
 
+import com.alibaba.dubbo.config.ProtocolConfig;
 import org.apache.commons.lang.SystemUtils;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -17,13 +18,20 @@ import static org.apache.commons.lang.SystemUtils.OS_NAME;
  * @create 2017/11/16 10:22
  */
 public class DubboMain {
+    private static final String ONE = "one";
+    private static final String TWO = "two";
     public static void main(String[] args){
-        ClassPathXmlApplicationContext context1 = new ClassPathXmlApplicationContext("applicationContext.xml","dubbo/dubbo_provider_1.xml");
-        context1.start();
-        System.out.println("Dubbo provider1 start...");
-        ClassPathXmlApplicationContext context2 = new ClassPathXmlApplicationContext("applicationContext.xml","dubbo/dubbo_provider_2.xml");
-        context2.start();
-        System.out.println("Dubbo provider2 start...");
+        if (args.length > 0){
+            if (args[0].equals(ONE)){
+                startProviderONE();
+            }
+            if (args[0].equals(TWO)){
+                startProviderTWO();
+            }
+        }else {
+            startProviderONE();
+            startProviderTWO();
+        }
         String OS_NAME = SystemUtils.OS_NAME;
         System.out.println("-----------------------------当前操作系统为 : " + OS_NAME + "---------------------------------");
         if (OS_NAME.indexOf("Windows") >= 0){
@@ -36,4 +44,24 @@ public class DubboMain {
             while (true){}
         }
     }
+
+    private static void startProviderONE(){
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml","dubbo/dubbo_provider_1.xml");
+        context.start();
+        ProtocolConfig protocolConfig = context.getBean(ProtocolConfig.class);
+        System.out.println("Dubbo provider start ==================>" + protocolConfig.toString());
+    }
+    private static void startProviderTWO(){
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml","dubbo/dubbo_provider_2.xml");
+        context.start();
+        ProtocolConfig protocolConfig = context.getBean(ProtocolConfig.class);
+        System.out.println("Dubbo provider start ==================>" + protocolConfig.toString());
+    }
+
+
+
+
+
+
+
 }
