237bc04819d89849f6fc39d982c5e8c3dd9a4d6b
zyouke <zyouke@aliyun.com>
2018-12-02 21:47:15 +0800

    nio学习

--- netty/src/main/java/com/zyouke/nio/NioChannel.java
+++ netty/src/main/java/com/zyouke/nio/NioChannel.java
@@ -1,14 +1,31 @@
 package com.zyouke.nio;
 
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
-import java.nio.file.Paths;
 
 public class NioChannel {
 
     public static void main(String[] args) throws Exception {
-        ByteBuffer buffer = ByteBuffer.allocate(1024);
-        FileChannel fileChannel = FileChannel.open(Paths.get("F:/temp/t_area.sql"));
+        ByteBuffer buffer = ByteBuffer.allocate(256);
+        FileInputStream inputStream = new FileInputStream("f:/temp/t_area.sql");
+        FileOutputStream outputStream = new FileOutputStream("f:/temp/user.txt");
+        FileChannel inputStreamChannel = inputStream.getChannel();
+        FileChannel outputStreamChannel = outputStream.getChannel();
+        while (true){
+            buffer.clear();
+            int read = inputStreamChannel.read(buffer);
+            System.out.println(read);
+            if (read < 0){
+                break;
+            }
+            buffer.flip();
+            System.out.println(new String(buffer.array(),"UTF-8"));
+            //outputStreamChannel.write(buffer);
+        }
+        inputStream.close();
+        outputStream.close();
     }
 }
