994d165e2f91a8a0ce11be88ba120a7708fa9352
zyouke <zyouke@aliyun.com>
2018-12-08 22:10:53 +0800

    nio编程开发

--- netty/src/main/java/com/zyouke/nio/NioSelector.java
+++ netty/src/main/java/com/zyouke/nio/NioSelector.java
@@ -1,45 +1,44 @@
 package com.zyouke.nio;
 
 import java.net.InetSocketAddress;
+import java.net.ServerSocket;
 import java.nio.ByteBuffer;
 import java.nio.channels.*;
+import java.nio.charset.Charset;
 import java.util.Iterator;
 import java.util.Set;
 
 public class NioSelector {
 
     public static void main(String[] args) throws Exception {
-        Selector selector = Selector.open();
         ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
         serverSocketChannel.configureBlocking(false);
-        serverSocketChannel.socket().bind(new InetSocketAddress(8080));
+        ServerSocket serverSocket = serverSocketChannel.socket();
+        serverSocket.bind(new InetSocketAddress(8080));
+        Selector selector = Selector.open();
         serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
         while (true){
-            Set<SelectionKey> selectionKeys = selector.keys();
-            Iterator<SelectionKey> keyIterator = selectionKeys.iterator();
-            while (keyIterator.hasNext()){
-                SelectionKey selectionKey = keyIterator.next();
+            selector.select();
+            Set<SelectionKey> selectionKeys = selector.selectedKeys();
+            for (SelectionKey selectionKey : selectionKeys) {
+                SocketChannel socketChannel;
                 if (selectionKey.isAcceptable()){
                     ServerSocketChannel channel = (ServerSocketChannel) selectionKey.channel();
-                    SocketChannel socketChannel = channel.accept();
+                    socketChannel = channel.accept();
                     socketChannel.configureBlocking(false);
                     socketChannel.register(selector,SelectionKey.OP_READ);
-                    keyIterator.remove();
-                    System.out.println("当前连接的客户端 ：" + channel);
-                }else if (selectionKey.isReadable()){
-                    SocketChannel socketChannel = (SocketChannel) selectionKey.channel();
-                    while (true){
-                        ByteBuffer buffer = ByteBuffer.allocate(1024);
-                        buffer.clear();
-                        int read = socketChannel.read(buffer);
-                        if (read < 0){
-                            break;
-                        }
+                }
+                if (selectionKey.isReadable()){
+                    ByteBuffer buffer = ByteBuffer.allocate(512);
+                    socketChannel = (SocketChannel) selectionKey.channel();
+                    int read = socketChannel.read(buffer);
+                    if (read > 0){
                         buffer.flip();
-                        System.out.println(new String(buffer.array(),"UTF-8"));
-                        socketChannel.write(buffer);
+                        Charset charset = Charset.forName("utf-8");
+                        System.out.println(socketChannel + ":" + String.valueOf(charset.decode(buffer).array()));
                     }
                 }
+                selectionKeys.remove(selectionKey);
             }
         }
     }
