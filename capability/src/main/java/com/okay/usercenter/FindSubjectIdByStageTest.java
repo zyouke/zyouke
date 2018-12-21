43aac4efee52e12b7b63af2fc1eb6d9013211cdf
zhoujun <zhoujun@okay.cn>
2018-12-21 11:59:38 +0800

    压力测试提交

--- capability/src/main/java/com/okay/usercenter/FindSubjectIdByStageTest.java
+++ capability/src/main/java/com/okay/usercenter/FindSubjectIdByStageTest.java
@@ -1,8 +1,10 @@
-package com.okay.usersvr;
+package com.okay.usercenter;
 
 import com.noriental.usersvr.bean.request.RequestLong;
 import com.noriental.usersvr.bean.user.domain.Student;
 import com.noriental.usersvr.service.okuser.student.StudentService;
+import com.noriental.utils.json.JsonUtil;
+import com.noriental.utils.servlet.HttpUtils;
 import com.noriental.validate.bean.CommonResponse;
 import com.taobao.stresstester.StressTestUtils;
 import com.taobao.stresstester.core.StressResult;
@@ -10,28 +12,24 @@ import com.taobao.stresstester.core.StressTask;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @Author: zhoujun
  */
-public class FindStudentBySystemIdTest {
+public class FindSubjectIdByStageTest {
     public static void main(String[] args) {
-        //加载dubbo配置文件
         ApplicationContext context = new ClassPathXmlApplicationContext(new String[] {"applicationContext-consumer-user.xml"});
-        //获取server
-        final StudentService studentService = (StudentService) context.getBean("studentService");
-
-        /**
-         * StressTestUtils.test(int concurrencyLevel, int totalRequests, StressTask stressTask, int warmUpTime);
-         * concurrencyLevel  总并发次数
-         * totalRequests 总请求次数， 单个线程请求次数= totalRequests/concurrencyLevel
-         * stressTask  并发主体
-         * warmUpTime  初始绿灯测试stressTask次数
-         */
-        StressResult stressResult = StressTestUtils.test(500, 3000, new StressTask() {
+        StudentService studentService = (StudentService) context.getBean("studentService");
+        StressResult stressResult = StressTestUtils.test(10, 500, new StressTask() {
             public Object doTask() throws Exception {
-                RequestLong requestLong = new RequestLong(81951061835L);
-                requestLong.setReqId("abc123456789");
-                CommonResponse<Student> commonResponse = studentService.findStudentBySystemId(requestLong);
+                Map<String,String> headerMap = new HashMap<String,String>();
+                headerMap.put("requestid","abc123");
+                headerMap.put("Content-Type","application/json;charset=utf-8");
+                Map<String, Object> entityMap = new HashMap<String, Object>(1);
+                entityMap.put("entity",2);
+                HttpUtils.doPost("http://127.0.0.1:28081/usercenter/user/findSubjectIdByStage", JsonUtil.obj2Json(entityMap),5000,5000,headerMap);
                 return null;
             }
         }, 0);
--- capability/src/main/java/com/okay/usersvr/FindStudentsTest.java
+++ capability/src/main/java/com/okay/usersvr/FindStudentsTest.java
@@ -1,7 +1,9 @@
 package com.okay.usersvr;
 
 import com.noriental.usersvr.bean.request.RequestLong;
+import com.noriental.usersvr.bean.request.UserBaseMapRequest;
 import com.noriental.usersvr.bean.user.domain.Student;
+import com.noriental.usersvr.service.okuser.UserService;
 import com.noriental.usersvr.service.okuser.student.StudentService;
 import com.noriental.validate.bean.CommonResponse;
 import com.taobao.stresstester.StressTestUtils;
@@ -10,15 +12,20 @@ import com.taobao.stresstester.core.StressTask;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  * @Author: zhoujun
  */
-public class FindStudentBySystemIdTest {
+public class FindStudentsTest {
     public static void main(String[] args) {
         //加载dubbo配置文件
         ApplicationContext context = new ClassPathXmlApplicationContext(new String[] {"applicationContext-consumer-user.xml"});
         //获取server
-        final StudentService studentService = (StudentService) context.getBean("studentService");
+        final UserService userService = (UserService) context.getBean("userService");
 
         /**
          * StressTestUtils.test(int concurrencyLevel, int totalRequests, StressTask stressTask, int warmUpTime);
@@ -27,16 +34,22 @@ public class FindStudentBySystemIdTest {
          * stressTask  并发主体
          * warmUpTime  初始绿灯测试stressTask次数
          */
-        StressResult stressResult = StressTestUtils.test(500, 3000, new StressTask() {
+        StressResult stressResult = StressTestUtils.test(500, 500, new StressTask() {
             public Object doTask() throws Exception {
-                RequestLong requestLong = new RequestLong(81951061835L);
-                requestLong.setReqId("abc123456789");
-                CommonResponse<Student> commonResponse = studentService.findStudentBySystemId(requestLong);
+                UserBaseMapRequest baseMapRequest = new UserBaseMapRequest();
+                Map params = new HashMap();
+                List<Long> list = new ArrayList<Long>();
+                list.add(12721L);
+                params.put("klassIds",list);
+                baseMapRequest.setParams(params);
+                baseMapRequest.setReqId("abc123456789");
+                userService.findStudent(baseMapRequest);
                 return null;
             }
         }, 0);
         //数据格式化
         String str = StressTestUtils.format(stressResult);
         System.out.println(str);
+
     }
 }
