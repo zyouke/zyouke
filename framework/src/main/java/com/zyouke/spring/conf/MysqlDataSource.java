36fe4a25fa800bb2c1789ce81e91c9032a1f991e
âzyouke <zyouke@aliyun.com>
2019-09-16 23:19:20 +0800

    spring 事务

--- framework/src/main/java/com/zyouke/spring/conf/MysqlDataSource.java
+++ framework/src/main/java/com/zyouke/spring/conf/MysqlDataSource.java
@@ -2,17 +2,21 @@ package com.zyouke.spring.conf;
 
 
 import com.mysql.jdbc.Driver;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.jdbc.datasource.SimpleDriverDataSource;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.support.TransactionTemplate;
 
 import javax.sql.DataSource;
 
 @Component
 public class MysqlDataSource {
+    @Autowired
+    private DataSource dataSource;
     @Bean
     public DataSource dataSource() {
         SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
@@ -25,8 +29,13 @@ public class MysqlDataSource {
 
     @Bean
     public JdbcTemplate jdbcTemplate(){
-        JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource());
+        JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
         return jdbcTemplate;
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager(){
+        return new DataSourceTransactionManager(dataSource);
     }
 
 }
