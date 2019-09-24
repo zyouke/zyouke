f2d1797a1848f76811c8d4a2450fd5ad597ae7fa
âzyouke <zyouke@aliyun.com>
2019-09-24 22:48:00 +0800

    插入排序练习

---
+++ java_base/src/com/zyouke/sort/InsertionSort.java
@@ -0,0 +1,22 @@
+package com.zyouke.sort;
+
+/**
+ * 插入排序
+ *1，从第一个元素开始，该元素可以认为已经被排序
+ *2，取出下一个元素，在已经排序的元素序列中从后向前扫描,
+ * 3，如果当前元素大于前一个元素则替换位置
+ */
+public class InsertionSort {
+
+    public static void main(String[] args){
+        int[] arr = {49,38,65,58,15,35,25,53,51};
+        // arr[i]
+        for(int i = 1; i < arr.length; i++){
+
+        }
+        for(int i : arr){
+            System.out.print(i);
+            System.out.print(",");
+        }
+    }
+}
