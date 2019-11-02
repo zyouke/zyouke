d4e9db622b73f55d4c90cda92c612b96ac8b54d1
âzyouke <zyouke@aliyun.com>
2019-11-02 23:42:48 +0800

    使用springboot

--- framework/src/main/java/com/zyouke/distributed_transactions/goods/OrderSpringBootApplication.java
+++ framework/src/main/java/com/zyouke/distributed_transactions/goods/OrderSpringBootApplication.java
@@ -1,11 +1,10 @@
 package com.zyouke.distributed_transactions.goods;
 
-import com.alibaba.dubbo.spring.boot.annotation.EnableDubboConfiguration;
+import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@EnableDubboConfiguration
 public class OrderSpringBootApplication {
 
     public static void main(String[] args) throws InterruptedException {
--- framework/src/test/java/com/zyouke/dubbo/base/DubboBaseTest.java
+++ framework/src/test/java/com/zyouke/dubbo/base/DubboBaseTest.java
@@ -3,12 +3,12 @@ package com.zyouke.dubbo.base;
 import com.alibaba.dubbo.common.Constants;
 import com.alibaba.dubbo.common.URL;
 import com.alibaba.dubbo.common.compiler.Compiler;
-import com.alibaba.dubbo.common.extension.ExtensionLoader;
 import com.alibaba.dubbo.registry.RegistryFactory;
 import com.alibaba.dubbo.rpc.Protocol;
 import com.alibaba.dubbo.rpc.ProxyFactory;
 import com.zyouke.dubbo.base.spi.animal.Animal;
 import com.zyouke.dubbo.base.spi.job.Job;
+import org.apache.dubbo.common.extension.ExtensionLoader;
 import org.junit.Test;
 
 import java.util.HashMap;
